d34bf9c87d034a275830ec345762460dc9366225
Júlia Morsch <juliamorsh17@gmail.com>
2024-08-04 19:54:36 -0300

    feat(heranca): adicionar heranca a classe LavaLouca.

--- lista_construtores_heranca/LavaLouca.java
+++ lista_construtores_heranca/LavaLouca.java
@@ -1,14 +1,11 @@
 package lista_construtores_heranca;
 
-public class LavaLouca {
+public class LavaLouca extends Eletrodomestico{
     
-    private String modelo;
-    private String fabricante;
     private double litros;
     
     public LavaLouca(String modelo, String fabricante, double litros) {
-        this.modelo = modelo;
-        this.fabricante = fabricante;
+        super(modelo, fabricante);
         this.litros = litros;
     }
 
