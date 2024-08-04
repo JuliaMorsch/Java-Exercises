4da7f9554c368875db3837c19d60391c96fae4f2
Júlia Morsch <juliamorsh17@gmail.com>
2024-08-04 19:53:07 -0300

    feat(heranca): adicionar heranca a classe Geladeira.

--- lista_construtores_heranca/Geladeira.java
+++ lista_construtores_heranca/Geladeira.java
@@ -1,13 +1,11 @@
 package lista_construtores_heranca;
 
-public class Geladeira {
-    private String modelo;
-    private String fabricante;
+public class Geladeira extends Eletrodomestico {
+
     private double litros;
    
     public Geladeira(String modelo, String fabricante, double litros) {
-        this.modelo = modelo;
-        this.fabricante = fabricante;
+        super(modelo, fabricante);
         this.litros = litros;
     }
 
