f95e1d7576a2556b5c05b2744ac7b0f09ba40139
Júlia Morsch <juliamorsh17@gmail.com>
2024-08-04 19:03:39 -0300

    feat(heranca):superclasse para heranca das subclasses.

---
+++ lista_construtores_heranca/Eletrodomestico.java
@@ -0,0 +1,17 @@
+package lista_construtores_heranca;
+
+public class Eletrodomestico {
+    
+    private String modelo;
+    private String fabricante;
+   
+    public Eletrodomestico(String modelo, String fabricante) {
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+    }
+
+    public Eletrodomestico() {
+    }
+
+    
+}
